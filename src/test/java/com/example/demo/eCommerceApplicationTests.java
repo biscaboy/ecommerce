d90d6d7055d888b32b1c238bf6b290d857055021
biscaboy <david@davidjdickinson.com>
2021-01-16 18:07:21 -0700

    test: add user integration tests

--- src/test/java/com/example/demo/eCommerceApplicationTests.java
+++ src/test/java/com/example/demo/eCommerceApplicationTests.java
@@ -1,15 +1,110 @@
 package com.example.demo;
 
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.requests.CreateUserRequest;
+import com.example.demo.security.LoginRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-import org.junit.jupiter.api.Test;
+import java.net.URI;
+import java.util.ArrayList;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@SpringBootTest(classes = eCommerceApplication.class)
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class eCommerceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext context;
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<CreateUserRequest> createJson;
+
+    @Autowired
+    private JacksonTester<User> userJson;
+
+    @Autowired
+    private JacksonTester<LoginRequest> loginJson;
+
+    @Autowired
+    private JacksonTester<ArrayList<User>> userListJson;
+
+    @Test
+    public void create_new_user() throws Exception {
+
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("BillyHoliday");
+        userRequest.setPassword("789yuiYUI&*(");
+        userRequest.setConfirmPassword("789yuiYUI&*(");
+
+        mvc.perform(post(new URI("/api/user/create"))
+                .content(createJson.write(userRequest).getJson())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("username").isNotEmpty())
+                .andExpect(jsonPath("username").value(userRequest.getUsername()))
+                .andExpect(jsonPath("id").isNotEmpty());
+    }
+
+    @Test
+    public void get_user_list_authorized() throws Exception {
+
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("AnotherInfoSeeker");
+        userRequest.setPassword("(*&&7898adlkfBEND)");
+        userRequest.setConfirmPassword("(*&&7898adlkfBEND)");
+
+        LoginRequest loginRequest = new LoginRequest();
+        BeanUtils.copyProperties(userRequest, loginRequest);
+
+        mvc.perform(post(new URI("/api/user/create"))
+                .content(createJson.write(userRequest).getJson())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+        String token = mvc.perform(post(new URI("/login"))
+                .content(loginJson.write(loginRequest).getJson())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getHeader("Authorization");
+
+        String responseContent = mvc.perform(
+                get(new URI("/api/user/list"))
+                        .content(createJson.write(userRequest).getJson())
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .header("Authorization", token)
+                        .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        ArrayList<User> users = userListJson.parse(responseContent).getObject();
+        assertEquals(userRequest.getUsername(), users.get(users.size() - 1).getUsername());
+    }
 }
--- src/test/java/com/example/demo/security/SecurityConfigurationTests.java
+++ src/test/java/com/example/demo/security/SecurityConfigurationTests.java
@@ -191,26 +191,4 @@ public class SecurityConfigurationTests {
         User user = userJson.parse(responseContent).getObject();
         assertEquals(userRequest.getUsername(), user.getUsername());
     }
-
-    private class LoginRequest {
-        private String username;
-        private String password;
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-    }
-
 }
